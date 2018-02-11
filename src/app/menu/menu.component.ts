963d49196784707cf49cd6826f1cd18e10d28af0
itamar zangvil <itamar.zangvil@gmail.com>
2018-02-11 17:32:29 +0200

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,38 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-const DISHES: Dish[] = [
-  {
-    name:'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    label:'Hot',
-    price:'4.99',
-    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
- {
-    name:'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    label:'',
-    price:'1.99',
-    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
- {
-    name:'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    label:'New',
-    price:'1.99',
-    description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
- {
-    name:'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    label:'',
-    price:'2.99',
-    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
- ];
-  
-
 import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
 @Component({
   selector: 'app-menu',
@@ -49,5 +18,7 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
+  }
 }
