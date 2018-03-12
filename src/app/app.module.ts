4b5504432e0679692d9630c7f7811acd4ce4f274
itamar zangvil <itamar.zangvil@gmail.com>
2018-03-12 19:55:26 +0200

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService} from './services/promotion.service'
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -41,7 +42,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+     PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
